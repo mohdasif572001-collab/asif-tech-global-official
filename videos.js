cb51a8a229181708e67d631b00207e226eccff4a
Asif6967 <mohdasif696788@gmail.com>
2026-04-26 23:01:17 +0530

    Organized videos into Tutorials, Shorts, and Playlists

--- videos.js
+++ videos.js
@@ -1,6 +1,13 @@
 // videos.js file
-const myVideos = [
-    "ScMzIvxBSi4", // Advanced Windows Optimization Guide
-    "5QrR9PZW_BI", // Aapki dusri video
-    "YRaIypeXVcY"  // Aapki nayi Shorts video (https://youtube.com/shorts/YRaIypeXVcY)
-];
+const myContent = {
+    tutorials: [
+        "ScMzIvxBSi4", //  "https://youtu.be/Gzy6k-7k5iA"
+        "5QrR9PZW_BI"  //  "https://youtu.be/5QrR9PZW_BI"
+    ],
+    shorts: [
+        "uCWigClzovY", //  "https://youtube.com/shorts/uCWigClzovY?feature=share"
+        "YRaIypeXVcY"  //  "https://youtube.com/shorts/YRaIypeXVcY?feature=share"
+    playlists: [
+        "UURfvXfU4toossqCIbuXNblA" // Aapki main uploads playlist
+    ]
+};
