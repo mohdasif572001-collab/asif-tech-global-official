894bcba38dbb42bff5463e2ae0ecdf84ce39a5ee
Asif6967 <mohdasif696788@gmail.com>
2026-04-26 22:45:19 +0530

    Added videos.js with all video IDs

---
+++ videos.js
@@ -0,0 +1,6 @@
+// videos.js file
+const myVideos = [
+    "ScMzIvxBSi4", // Advanced Windows Optimization Guide
+    "5QrR9PZW_BI", // Aapki dusri video
+    "YRaIypeXVcY"  // Aapki nayi Shorts video (https://youtube.com/shorts/YRaIypeXVcY)
+];
