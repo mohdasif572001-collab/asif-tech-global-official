fb4d69ea72aeac9144e993c022992e24556e44f7
Asif6967 <mohdasif696788@gmail.com>
2026-04-26 23:42:30 +0530

    Final fix for professional video section

--- videos.js
+++ videos.js
@@ -1,13 +1,5 @@
-// videos.js file
 const myContent = {
-    tutorials: [
-        "ScMzIvxBSi4", //  "https://youtu.be/Gzy6k-7k5iA"
-        "5QrR9PZW_BI"  //  "https://youtu.be/5QrR9PZW_BI"
-    ],
-    shorts: [
-        "uCWigClzovY", //  "https://youtube.com/shorts/uCWigClzovY?feature=share"
-        "YRaIypeXVcY"  //  "https://youtube.com/shorts/YRaIypeXVcY?feature=share"
-    playlists: [
-        "UURfvXfU4toossqCIbuXNblA" // Aapki main uploads playlist
-    ]
+    tutorials: ["ScMzIvxBSi4", "5QrR9PZW_BI"],
+    shorts: ["uCWigClzovY", "YRaIypeXVcY"],
+    playlists: ["UURfvXfU4toossqCIbuXNblA"]
 };
