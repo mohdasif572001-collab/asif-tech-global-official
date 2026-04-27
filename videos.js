b54fdad02efa97f0c7fb1a37578e015be615c390
Asif6967 <mohdasif696788@gmail.com>
2026-04-27 14:15:31 +0530

    Updated videos data

--- videos.js
+++ videos.js
@@ -1,5 +1,7 @@
+// videos.js file
 const myContent = {
     tutorials: ["ScMzIvxBSi4", "5QrR9PZW_BI"],
     shorts: ["uCWigClzovY", "YRaIypeXVcY"],
+    pastLives: ["ScMzIvxBSi4"], // Jo live khatam ho jaye, uski ID yahan dalte rahein
     playlists: ["UURfvXfU4toossqCIbuXNblA"]
 };
