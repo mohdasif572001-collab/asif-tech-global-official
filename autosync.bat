9562bf3658e015705df6ceb1b57a915c1691d18e
Mohd Asif <mohdasif572001@gmail.com>
2026-04-28 15:16:54 +0530

    Auto-sync folders updated

--- autosync.bat
+++ autosync.bat
@@ -1,6 +1,10 @@
 @echo off
 title Asif Tech - Live Auto Sync
 color 0A
+
+:: 👇 Ye line batayegi ki folder kahan hai (Double-click fix)
+cd /d "%~dp0"
+
 echo ===================================================
 echo   ASIF TECH GLOBAL - BACKGROUND AUTO-SYNC ACTIVE
 echo ===================================================
